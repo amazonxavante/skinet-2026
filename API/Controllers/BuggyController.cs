6f717e524c2f8e427adf93641c63aa0e7a61301d
William Vitorino <williamvitorino@hotmail.com>
2026-03-02 11:43:13 +1100

    End Section 6

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,43 @@
+using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseAPIController
+    {
+        [HttpGet("unauthorized")]
+        public ActionResult<string> GetUnauthorized()
+        {
+            return Unauthorized("You are not authorized to access this resource.");
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This is a bad request.");
+        }
+
+
+        [HttpGet("NotFound")]
+        public ActionResult<string> GetNotFound()
+        {
+            return NotFound("The requested resource was not found.");
+        }
+
+        [HttpGet("internalerror")]
+        public ActionResult<string> GetInternalError()
+        {
+            throw new Exception("This is an internal server error.");
+        }
+
+        [HttpPost("validationerror")]
+        public ActionResult<string> GetValidationError(CreateProductDto product)
+        {
+            return Ok();
+        }
+
+        
+    }
+}
+
