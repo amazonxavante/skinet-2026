8a4e9fc5367de01a4bb3dbf20f3f81e7bc7997fa
William Vitorino <williamvitorino@hotmail.com>
2026-04-01 12:58:06 +1100

    End of Section 12

---
+++ Core/Interfaces/ICartService.cs
@@ -0,0 +1,11 @@
+namespace Core.Interfaces
+{
+    using Core.Entities;
+
+    public interface ICartService
+    {
+        Task<ShoppingCart?> GetCartAsync(string key);
+        Task<ShoppingCart?> SetCartAsync(ShoppingCart cart);
+        Task<bool> DeleteCartAsync(string key);
+    }
+}
